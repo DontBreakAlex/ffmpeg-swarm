5998959bb0d73649e4f85afca77dfc3cbd9dbd27
Alexandre Seo <pro@alexandre-seo.com>
2023-07-11 17:56:52 +0200

    Make server a command

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,15 @@ use service::{install_service, uninstall_service};
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    #[arg(long, default_value = "false")]
-    server: bool,
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    Install {},
-    Uninstall {},
+    Server,
+    Install,
+    Uninstall,
     #[command(trailing_var_arg = true)]
     Submit {
         args: Vec<String>,
@@ -28,19 +27,14 @@ enum Commands {
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
-    // println!("{:#?}", cli);
-
-    if cli.server {
-        server::run()?;
-    }
 
     match cli.command {
-        Some(Commands::Install {}) => install_service(),
-        Some(Commands::Uninstall {}) => uninstall_service(),
-        Some(Commands::Submit { args }) => {
+        Commands::Install => install_service(),
+        Commands::Uninstall => uninstall_service(),
+        Commands::Submit { args } => {
             cli::submit(args)?;
         }
-        None => {}
+        Commands::Server => server::run()?,
     }
 
     Ok(())
